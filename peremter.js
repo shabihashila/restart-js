2f7feea843355d97f27dd040fe4c16821eaf47c1
HP <HP@DESKTOP-8E0FCDH>
2024-12-14 00:37:54 +0600

    peremter.js

--- peremter.js
+++ peremter.js
@@ -1,10 +1,28 @@
-function vatkhao() {  
-    // 1 no line e function declear krlm
-  console.log("i am depression");
-  console.log("what can i do");
-  console.log("pls anylone help me");
-  console.log("motivated me plz");
+// function vatkhao() {
+//   // 1 no line e function declear krlm
+//   console.log("ami vhat khay");
+//   console.log("what can i do");
+//   console.log("tumi ki khao");
+//   console.log("more jao");
+// }
+// // 8 no line e function call krlm
+// // ekta function er power hcce bibinno jinis diye use krte pari
+
+// vatkhao();
+
+// ekta function declear korbo er maddhome
+
+// function square(number) {
+//   // first bracket er fake ek ba ekadik ,jare koi peremeter,,nam declear krte pari
+//   //   console.log(number);
+//   const borgo = number + number;
+//   console.log(borgo);
+// }
+// square(10);
+// square(7);
+// square(12);
+function add(num) {
+  const shila = num * num;
+  console.log(shila);
 }
-// 8 no line e function call krlm
-// ekta function er power hcce bibinno jinis diye use krte pari
-vatkhao();
+add(5);
