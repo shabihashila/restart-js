c75de7ba95d2b093f23ff28c5b2c2f7ee9b1a05a
HP <HP@DESKTOP-8E0FCDH>
2024-12-13 22:20:08 +0600

    peremeter.js

---
+++ peremter.js
@@ -0,0 +1,10 @@
+function vatkhao() {  
+    // 1 no line e function declear krlm
+  console.log("i am depression");
+  console.log("what can i do");
+  console.log("pls anylone help me");
+  console.log("motivated me plz");
+}
+// 8 no line e function call krlm
+// ekta function er power hcce bibinno jinis diye use krte pari
+vatkhao();
