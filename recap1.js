6e34f9a0bde23e898a62351b2533c3367c07f30d
HP <HP@DESKTOP-8E0FCDH>
2025-02-06 15:31:25 +0600

    recap1.js

---
+++ recap1.js
@@ -0,0 +1,16 @@
+// function add(math1, math2) {
+//   return math1 + math2;
+// }
+// add(11, 12);
+// const reused = add(11, 12);
+// console.log(reused);
+// function math(sum1,sum2){
+// }
+function doMath(math1, math2) {
+  const HM = math1 * math2;
+  const HM2 = HM * 5;
+  return HM2;
+}
+// doMath(40, 20);
+const resued = doMath(40, 20);
+console.log(resued);
